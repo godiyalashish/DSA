5b5b38e9c456557e6bc855adda438cddfd4475a8
godiyalashish <ashishgodiyal.333@gmail.com>
2022-03-09 11:19:58 +0530

    DSA patterns

---
+++ patterns/pattern 9.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n= scn.nextInt();
+
+        for(int r = 1; r<=n; r++){
+            for(int c=1; c<=n; c++){
+                if(c==r || c+r==n+1){
+                    System.out.print("*\t");
+                }else{
+                    System.out.print("\t");
+                }
+            }
+        System.out.println();
+        }
+        
+
+    }
+}
