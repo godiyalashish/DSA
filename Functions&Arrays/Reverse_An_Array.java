ead02b7cec118654180e14aebe63d91f709badf5
godiyalashish <ashishgodiyal.333@gmail.com>
2022-03-14 21:30:59 +0530

    Functions & Arrays

---
+++ Functions&Arrays/Reverse_An_Array.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+  public static void display(int[] a){
+    StringBuilder sb = new StringBuilder();
+
+    for(int val: a){
+      sb.append(val + " ");
+    }
+    System.out.println(sb);
+  }
+
+  public static void reverse(int[] a){
+    int l=0,r=a.length-1,temp=0;
+    while(r>l){
+      temp=a[l];
+      a[l]=a[r];
+      a[r]=temp;
+      r--;
+      l++;
+    }
+  }
+
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] a = new int[n];
+    for(int i = 0; i < n; i++){
+       a[i] = Integer.parseInt(br.readLine());
+    }
+
+    reverse(a);
+    display(a);
+ }
+
+}
