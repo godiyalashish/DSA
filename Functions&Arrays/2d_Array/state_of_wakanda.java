1f34a81518678b931fe99aa951b364fb02879c8b
godiyalashish <ashishgodiyal.333@gmail.com>
2022-04-01 20:00:48 +0530

    state of wakanda

---
+++ Functions&Arrays/2d_Array/state_of_wakanda.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int r= scn.nextInt();
+    int c = scn.nextInt();
+
+    int [][] arr = new int[r][c];
+
+    for(int i=0; i<arr.length; i++){
+        for(int j=0; j<arr[0].length; j++){
+            arr[i][j] = scn.nextInt();
+        }
+    }
+    int row=0, col=0, dir = 0; //dir =0 means moving downwards
+    while(row<arr.length && col<arr[0].length){
+        if(dir == 0){
+            for( row = 0; row<arr.length; row++){
+                System.out.println(arr[row][col]);
+            }
+            row--;
+            col++;
+            dir =1;
+        }
+        else{//move upwards
+            for(row = arr.length-1; row>=0; row--){
+                System.out.println(arr[row][col]);
+            }
+            col++;
+            dir = 0;
+        }
+    }
+ }
+
+}
