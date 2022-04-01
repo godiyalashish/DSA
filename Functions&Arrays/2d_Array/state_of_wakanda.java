9285293b1d1173c40158d272798c518cfa72b00c
godiyalashish <ashishgodiyal.333@gmail.com>
2022-04-01 22:04:42 +0530

    state of wakanda-1

--- Functions&Arrays/2d_Array/state_of_wakanda.java
+++ Functions&Arrays/2d_Array/state_of_wakanda.java
@@ -36,3 +36,43 @@ public static void main(String[] args) throws Exception {
  }
 
 }
+
+
+//second approach
+
+
+// import java.io.*;
+// import java.util.*;
+
+// public class Main{
+
+// public static void main(String[] args) throws Exception {
+//     Scanner scn = new Scanner(System.in);
+//     int r= scn.nextInt();
+//     int c = scn.nextInt();
+
+//     int [][] arr = new int[r][c];
+
+//     for(int i=0; i<arr.length; i++){
+//         for(int j=0; j<arr[0].length; j++){
+//             arr[i][j] = scn.nextInt();
+//         }
+//     }
+//     int row=0, col=0;
+//     while(col<arr[0].length){
+//         if(col%2 ==  0){
+//             for( row = 0; row<arr.length; row++){
+//                 System.out.println(arr[row][col]);
+//             }
+//             col++;
+//         }
+//         else{
+//             for(row = arr.length-1; row>=0; row--){
+//                 System.out.println(arr[row][col]);
+//             }
+//             col++;
+//         }
+//     }
+//  }
+
+// }
