e951435558e27293782f3bb15d759bfc9b7300d3
godiyalashish <ashishgodiyal.333@gmail.com>
2022-05-15 22:59:34 +0530

    bubble & selection sort

---
+++ sorting/Main.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+       bubbleSort(nums);
+       return nums;
+    }
+    public static void swap(int nums[],int i,int j){
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+    public static void selectionSort(int nums[]){
+        int n = nums.length;
+        for(int itr = 1 ; itr <= n-1 ; itr++){
+            int mIdx = itr-1;
+            for(int i = itr ; i < n ; i++){
+                if(nums[mIdx] > nums[i]) mIdx = i;
+            }
+            swap(nums, mIdx, itr-1);
+        }
+    }
+
+    public static void bubbleSort(int nums[]){
+        int n = nums.length;
+        for(int itr = 1 ; itr <= n-1 ; itr++){
+            for(int i = 0 ; i <= n - 1 - itr ; i++){
+                if(nums[i+1] < nums[i]){
+                    swap(nums, i, i+1);
+                }
+            }
+        }
+    }
+}
