9f3d9246f5e56092c0a3d0531d0d88fee8df9080
godiyalashish <ashishgodiyal.333@gmail.com>
2022-07-03 13:22:41 +0530

    get common elements & intersection of two arrays

---
+++ hashmap&heap/GetCommonElements-2.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void findCommEle(int[] a1, int[] a2){
+    HashMap <Integer,Integer> hm = new HashMap<>();
+
+    for(int i: a1){
+        if(hm.containsKey(i)){
+          hm.put(i, hm.get(i)+1);
+        }else{
+          hm.put(i,1);
+        }
+    }
+
+    for(int i: a2){
+        if(hm.containsKey(i)){
+            System.out.println(i);
+            int updval = hm.get(i)-1;
+            if(updval == 0){
+              hm.remove(i);
+            }else{
+              hm.put(i, updval);
+            }
+        }
+    }
+}
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner scn = new Scanner(System.in);
+    int n1 = scn.nextInt();
+
+    int arr1 []  = new int[n1];
+    for(int i=0; i<n1; i++){
+        arr1[i] = scn.nextInt();
+    }
+
+    int n2 = scn.nextInt();
+
+    int arr2 []  = new int[n2];
+    for(int i=0; i<n2; i++){
+        arr2[i] = scn.nextInt();
+    }
+
+    findCommEle(arr1, arr2);
+ }
+
+}
