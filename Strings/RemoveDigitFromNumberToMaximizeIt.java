94e942a96104c5523fe6ff30ddac50842d6b5af7
godiyalashish <ashishgodiyal.333@gmail.com>
2022-09-09 12:55:30 +0530

    string

---
+++ Strings/RemoveDigitFromNumberToMaximizeIt.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String removeDigit(String number, char digit) {
+        for(int i=0; i<number.length()-1; i++){
+            if(number.charAt(i) == digit){
+                if(number.charAt(i+1) > digit){
+                    return (number.substring(0,i) + number.substring(i+1));
+                }
+            } 
+           
+        }
+        int lastIndex =  number.lastIndexOf(digit);
+        return (number.substring(0,lastIndex) + number.substring(lastIndex+1));
+    }
+}
