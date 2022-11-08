7594ab90745890d2375571ffbf70d73a34c94c2d
godiyalashish <ashishgodiyal.333@gmail.com>
2022-11-09 00:00:30 +0530

    graph

---
+++ graphs/NumberOfIslands.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                if(grid[i][j] == '1'){
+                    count++;
+                    dfs(grid,i,j);
+                }
+            }
+        }
+        return count;
+    }
+    
+    public void dfs(char[][] grid, int r, int c){
+        grid[r][c] = '0';
+        if(r-1>=0 && grid[r-1][c] == '1'){
+            dfs(grid, r-1, c);
+        }
+        
+        if(c+1<grid[0].length && grid[r][c+1] == '1'){
+            dfs(grid, r, c+1);
+        }
+        
+        if(r+1<grid.length && grid[r+1][c] == '1'){
+            dfs(grid, r+1, c);
+        }
+        if(c-1 >= 0 && grid[r][c-1] == '1'){
+            dfs(grid, r, c-1);
+        }
+        
+    }
+}
